7bd08f2808c7ad995e8492a964530c9bfe6cc8a6
Lucky <luckybaraka21@gmail.com>
2024-03-06 13:55:28 +0300

    console.py

--- console.py
+++ console.py
@@ -1,44 +1,65 @@
 #!/usr/bin/python3
-"""Define the HBnB console"""
-
+"""Defines the HBnB console."""
 import cmd
 import re
 from shlex import split
 
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
-    s_brackets = re.search(r"\[(.*?)\]", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
     if curly_braces is None:
-        if s_brackets is None:
+        if brackets is None:
             return [i.strip(",") for i in split(arg)]
         else:
             lexer = split(arg[:brackets.span()[0]])
             retl = [i.strip(",") for i in lexer]
             retl.append(brackets.group())
             return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
-        else:
-            lexer = split(arg[:curly_braces.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
 
-class hbnb_commandline(cmd.Cmd):
-    """This is our command line terminal.
-        Attributes:
-            prompt(str): This is the command prompt.
+class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command interpreter.
+    Attributes:
+        prompt (str): The command prompt.
     """
-    prompt = "(hbnb)"
-    _classes = {
-            "BaseModel",
-            "User",
-            "State",
-            "City",
-            "Place",
-            "Amenity",
-            "Reviews"
-            }
 
-    def empty_line(self):
-        """ It does nothing on the empty input. """
+    prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
         pass
+
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
